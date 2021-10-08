a2547b2b83eaca865c289ec58e6360127f9825de
dev@nshu <im2tefte@gmail.com>
2021-10-08 16:32:51 +0530

    review changes

--- src/sharp1.ts
+++ src/sharp1.ts
@@ -1,17 +1,19 @@
 import sharp from 'sharp';
-import path, { resolve } from 'path';
+import path from 'path';
 
-const sharp1 = async (filename: string): Promise<void> => {
-  await sharp(path.join(__dirname, '../images', filename)).toFile(
-    path.join(__dirname, '../src/cache', filename),
-    err => {
-      if (err) {
-        console.log(err, 'sharp1 inside i am');
-      } else {
-        resolve('ok');
+const sharp1 = async (filename: string): Promise<unknown> => {
+  return new Promise(resolve => {
+    return sharp(path.join(__dirname, '../images', filename)).toFile(
+      path.join(__dirname, '../src/cache', filename),
+      err => {
+        if (err != null) {
+          console.log(err);
+          resolve(false);
+        }
+        return resolve(true);
       }
-    }
-  );
+    );
+  });
 };
 
 export default sharp1;
